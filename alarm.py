c000f5450f7056dc411abbc7a073bf2f40546972
sahilmohile15 <sahilmohile96@gmail.com>
2020-07-01 13:24:30 +0530

    Added Times and Creating Layout for Clock

--- alarm.py
+++ alarm.py
@@ -1,6 +1,37 @@
 #!/usr/bin/python
 
 import os
-from tkinter import *
+import tkinter as tk
+import datetime as dt
 from playsound import playsound
+import array
 
+#Creating Window and Label
+window = tk.Tk()
+title = tk.Label(text="Alarm Clock")
+title.pack()
+
+t1 = dt.time(hour=13,minute=15)
+t2 = dt.time(hour=12,minute=54)
+timetable = [t1, t2]
+
+
+#Adding a button to stop time
+sButton = tk.Button(
+    text="Stop",
+    bg = "blue",
+    fg = "red",
+    width = 15,
+    height= 3,
+)
+
+sButton.pack()
+
+window.mainloop()
+
+
+while True:
+    for i in timetable:
+        if i.hour == dt.datetime.now().hour and i.minute == dt.datetime.now().minute:
+            print("Its working...")
+            break
