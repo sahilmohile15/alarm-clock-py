9d35f3efc4abf174a8f1f6e300e5746b06a9cf5a
sahilmohile15 <54280984+sahilmohile15@users.noreply.github.com>
2020-10-31 16:03:24 +0530

    Update install.sh
    Update tkinter commands

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!bin/bash
 sudo apt install python
 
+sudo apt install python3-tk
+
 sudo apt install python3-pip
 
 pip3 install -r requirements.txt
 
-chomod +x alarm.sh
+chmod +x alarm.py
+
+python alarm.py
